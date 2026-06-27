f9a6af2cdcc233f279583f41c59faf16db8ef57e
Umme Abira Azmary <95493187+Mouly22@users.noreply.github.com>
2026-06-27 20:03:40 +0600

    Day 10: Export BanglaEmbedder from __init__.py

--- bangla_text_toolkit/__init__.py
+++ bangla_text_toolkit/__init__.py
@@ -3,6 +3,7 @@ from .stemmer import BanglaStemmer
 from .vectorizer import BanglaVectorizer
 from .keyword_extractor import BanglaKeywordExtractor
 from .sequence_labeler import BanglaSequenceLabeler
+from .embedder import BanglaEmbedder
 
 __version__ = "0.1.0"
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "BanglaVectorizer",
     "BanglaKeywordExtractor",
     "BanglaSequenceLabeler",
+    "BanglaEmbedder",
     "__version__",
 ]
